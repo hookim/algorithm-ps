506e5fbfc52a651fb035233039e5f3b2263010b3
hookim <81621620+hookim@users.noreply.github.com>
2024-06-08 20:00:20 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 492 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector> 
+#include <algorithm>
+
+using namespace std;
+
+long long N, a;
+vector<int> arr, lis;
+
+void searchAndReplace(int x){
+    int b = 0, e = lis.size()-1, m = (b+e)/2;
+    while(b < e){
+        m = (b+e) / 2;
+
+        if(lis[m] >= x){
+            e = m;
+        }
+        else{
+            b = m+1;
+        }
+
+    }
+    lis[e] = x;
+} 
+
+void solve(){
+    lis.push_back(arr[0]);
+
+    for(int i = 1; i < N; i++){
+        if(lis[lis.size()-1] < arr[i]){
+            lis.push_back(arr[i]);
+        }
+        else{
+            searchAndReplace(arr[i]);
+        }
+    }
+}
+
+int main(void){    
+    cin >> N;
+
+    for(int i = 0; i < N; i++){
+        cin >> a;
+        arr.push_back(a);
+    }
+    
+    solve();
+    cout << lis.size() << endl;
+
+    return 0;
+}
+
