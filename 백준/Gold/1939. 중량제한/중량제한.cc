70cce1056cfaff7c1adb207359b1691533055d36
hookim <81621620+hookim@users.noreply.github.com>
2024-06-09 17:49:37 +0900

    [Gold III] Title: 중량제한, Time: 140 ms, Memory: 7636 KB -BaekjoonHub

--- 백준/Gold/1939. 중량제한/중량제한.cc
+++ 백준/Gold/1939. 중량제한/중량제한.cc
@@ -8,25 +8,24 @@ using namespace std;
 
 int  N, M, a, s, e, b, w, idealWeight;
 vector<pair<int, int>> graph[100001];
-int maxWeights[100001];
+bool visited[100001];
 
 bool cmp(const pair<int, int>& a, const pair<int, int>& b){
     return a.second > b.second;
 }
 
 bool bfs(int minimum){
-     queue<pair<int, int>>q;
+     queue<int>q;
 
     for(auto v : graph[s]){
-        if(maxWeights[v.first] < v.second && v.second >= minimum){
-            maxWeights[v.first] = v.second;
-            q.push({v.first, v.second});
+        if(!visited[v.first] && v.second >= minimum){
+            visited[v.first] = true;
+            q.push(v.first);
         }
     }
 
     while(!q.empty()){
-        int curV = q.front().first;
-        int curW = q.front().second;
+        int curV = q.front();
         q.pop();
 
         if(curV == e){
@@ -34,15 +33,12 @@ bool bfs(int minimum){
         }
 
         for(auto v : graph[curV]){
-            int newV = v.first;
-            int newW = min(curW, v.second);
-
-            if(maxWeights[newV] >= newW || newW < minimum){
+            if(visited[v.first] || v.second < minimum){
                 continue;
             }
 
-            maxWeights[newV] = newW;
-            q.push({newV, newW});
+            visited[v.first] = true;
+            q.push(v.first);
         }
     }
 
@@ -56,7 +52,7 @@ void solve(){
         int m = low + (high - low) / 2;
         
         for(int i = 0; i <= N; i++){
-            maxWeights[i] = 0;
+            visited[i] = false;
         }
 
         if(bfs(m)){
